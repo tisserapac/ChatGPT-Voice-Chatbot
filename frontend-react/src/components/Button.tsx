0119c1f0a93a88e3f231e1c56c9cf152e16efbb4
Anjana Tissera <tisserapac@yahoo.com>
2024-06-03 21:49:24 +0700

    Bet box app - version 1

--- frontend-react/src/components/Button.tsx
+++ frontend-react/src/components/Button.tsx
@@ -7,7 +7,7 @@ function Button({ runFunction }: Props) {
     <>
       <button
         onClick={runFunction}
-        className="transition-all duration-200 border border-black w-full px-4 py-12 rounded-sm bg-indigo-500 hover:bg-indigo-600 text-white"
+        className="transition-all duration-200 border border-gray-200 w-full px-4 py-12 rounded-xl bg-indigo-500 hover:bg-indigo-600 text-white"
       >
         Place Bet
       </button>
